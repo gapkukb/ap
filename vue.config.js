5ea3263fafd6633b11942ed373f99b962bd3f465
AklanJoo <gapkukb@gmail.com>
2023-12-27 21:48:48 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,12 @@
 const { defineConfig } = require('@vue/cli-service');
 const path = require('path');
-const VueMacros = require('unplugin-vue-macros/webpack');
-
 const svgPath = path.resolve(__dirname, 'src/assets/svg');
+const AutoImport = require('unplugin-auto-import/webpack').default;
+const Components = require('unplugin-vue-components/webpack').default;
+const { VantResolver } = require('unplugin-vue-components/resolvers');
+
+console.log(AutoImport);
+
 module.exports = defineConfig({
   transpileDependencies: true,
 
@@ -19,5 +23,21 @@ module.exports = defineConfig({
       .options({
         symbolId: 'icon-[name]',
       });
+  },
+  configureWebpack: {
+    plugins: [
+      AutoImport({
+        imports: ['vue', 'vue-router', 'vuex', 'vue-router/composables'],
+        resolvers: [VantResolver()],
+      }),
+      Components({
+        // 要搜索组件的目录的相对路径
+        dirs: ['src/components', 'src/views'],
+        // 搜索子目录
+        deep: true,
+        extensions: ['vue', 'js', 'jsx', 'ts', 'tsx'],
+        include: [/\.vue$/, /\.vue\?vue/, /\.js$/, /\.jsx$/, /\.ts$/, /\.tsx$/],
+      }),
+    ],
   },
 });
